139338ac6f66d173b61d7eebb301b0f7b16b3d98
Rafael Ferraz <rafael.vitame@aluno.senai.br>
2025-10-10 16:31:59 -0300

    introdução
    introdução e exemplos php

---
+++ Aula1/exemplo1.php
@@ -0,0 +1,14 @@
+<?php
+    // geração de texto (string)
+    echo "chama sesi-senai!";
+
+    /*
+    geração de textoestruturado
+    com tags e atributos
+    */
+    // Aspas simples dentro das aspas duplas
+    echo "<h2>gerando <span class=\'destaque'>HTML</span> através do PHP.</h2>";
+
+ //Aspas duplas dentro de aspas dupla
+ echo "<h2>gerando <span class=\'destaque'>HTML</span> através do PHP.</h2>";
+?>
---
+++ Aula1/exemplo3.php
@@ -0,0 +1,10 @@
+<?php
+echo                            "testando espaço em branco <br>";
+echo "testando espaço <br>"           ;
+
+  // o caracter &nbsp adiciona espaço em branco dentro da string
+  echo "&#160; &nbsp;      testando            espaço em branco";
+  echo
+  "quebra de linha"
+  ;
+?>
